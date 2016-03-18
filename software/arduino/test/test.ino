a40e114845bba4239c930f3bdac5661db1e2469c
Bart Schapendonk <bart.schapendonk@topicus.nl>
2016-03-18 20:50:21 +0100

    inital scaffolding for an post assembly shield test programm

---
+++ software/arduino/test/test.ino
@@ -0,0 +1,15 @@
+// APA102 use the Rainbow example sketch
+// Button push turns on the led
+// TEMT6000 output the analogRead value to the serial monitor
+// LM75B output the temperature to the serial monitor
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
