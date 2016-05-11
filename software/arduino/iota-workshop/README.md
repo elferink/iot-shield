b48a300ad7fb928277551ce0101a61a8fe1ce671
Jeroen Idserda <jeroen.idserda@gmail.com>
2016-05-11 22:07:13 +0200

    Workshop voorbeeldsketch

---
+++ software/arduino/iota-workshop/README.md
@@ -0,0 +1,41 @@
+## Workshop Internet of Things
+
+Volg deze handleiding om snel wat leuks met je iota bordje te kunnen ontwikkelen! 
+
+**Voor de workshop**
+
+Installeer de volgende software **voordat de workshop begint**:
+
+- Download en installeer de laatste Arduino IDE (nu 1.6.9) voor jouw OS. Link: [https://www.arduino.cc/en/Main/Software](https://www.arduino.cc/en/Main/Software)
+- Installeer de driver voor de USB-chip van het ESP8266 bordje (alleen Mac en Windows). Deze zijn hier te vinden: [http://www.wemos.cc/downloads/](http://www.wemos.cc/downloads/)
+- Start de Arduino IDE en ga `Arduino`, `Preferences` en vul bij `Additional Boards Manager URLs` dit in: `http://arduino.esp8266.com/stable/package_esp8266com_index.json`
+- Ga naar `Tools`, `Board: ..`, `Boards Manager` en zoek hier op 'esp8266'. Installeer de laatste versie (2.2.0).
+- We hebben een testsketch gemaakt, download deze hier: <linkje>. Open deze vervolgens in de Arduino IDE. 
+- Voor deze sketch heb je een aantal libraries nodig. Download de volgende ZIP-files en voeg deze toe aan de Arduino IDE via `Sketch`, `Include library`, `Add .ZIP Library`. 
+	- [LM75 Arduino Library](https://github.com/thefekete/LM75/archive/master.zip)
+	- [FastLED](https://github.com/FastLED/FastLED/archive/87211a5a7c2e143b9d172d9ce62151049eb7b7b2.zip)
+	- [OneButton](https://github.com/mathertel/OneButton/archive/master.zip)
+- Compile de sketch via `Sketch`, `Verify/Compile`. Dit zou nu moeten lukken!
+- Tot op de workshop!
+
+**Tijdens de workshop**
+
+Als je eenmaal in het bezit bent van een iota bordje sluit je deze via USB aan. Selecteer vervolgens onder `Tools` de volgende opties (vanaf `Board`):
+
+![Alt text](../../../assets/settings-tools.png?raw=true)
+
+De waarde voor `port` zal per OS verschillen. 
+
+Vervolgens kun je via `Sketch`, `Upload` je code in de ESP8266 laden. 
+
+De testsketch verbindt met wifi en toont initieel de waarde van de twee sensoren. Met de knop je kun je het volgende:
+
+* 1x klikken: led aan/uit
+* dubbelklikken: rgb leds aan/uit
+* vasthouden: sensorwaarden tonen
+
+Succes!
+
+**Tips**
+
+In de Arduino IDE vind je onder `File`, `Examples` meer voorbeelden van de gebruikte libraries. O.a. `ESP8266HTTPClient` is handig om simpele HTTP requests te doen.
---
+++ software/arduino/iota-workshop/iota-workshop.ino
@@ -0,0 +1,166 @@
+#include <ESP8266WiFi.h>
+#include "OneButton.h"
+#include <Wire.h>
+#include <LM75.h>
+#include <FastLED.h>
+
+/**
+   Config, pas onderstaande waardes aan!
+*/
+#define WIFI_SSID           "My Wifi"
+#define WIFI_PASSWORD       "My Password"
+#define RGB_LED_BRIGHTNESS  32 // 0-255
+
+/*
+   Constanten
+*/
+#define BUTTON_PIN          0   // D3
+#define LED_PIN             2   // D4
+
+#define RGB_LED_DATA_PIN    13  // D7
+#define RGB_LED_CLOCK_PIN   14  // D5
+#define RGB_NUM_LEDS        2
+#define RGB_LED_TYPE        APA102
+#define RGB_COLOR_ORDER     BGR
+
+/*
+     Runtime variabelen
+*/
+OneButton button(BUTTON_PIN, true);
+LM75 temp_sensor;
+CRGB leds[RGB_NUM_LEDS];
+
+boolean led_on = false;
+boolean rgb_leds_on = false;
+
+/*
+   Functies om gebruik van de knop af te handelen
+*/
+void singleClick() {
+  led_on = !led_on;
+  setLed(led_on);
+}
+
+void doubleClick() {
+  rgb_leds_on = !rgb_leds_on;
+
+  if (rgb_leds_on) {
+    setRgbLeds(CRGB::Green);
+  } else {
+    setRgbLeds(CRGB::Black);
+  }
+}
+
+void longPress() {
+  getTemperature();
+  getLuminance();
+}
+
+/*
+   Temperatuursensor
+*/
+void getTemperature() {
+  // Sensor activeren
+  temp_sensor.shutdown(false);
+  delay(100);
+
+  float temperature = temp_sensor.temp();
+  Serial.print("Temperatuur: ");
+  Serial.print(temperature);
+  Serial.println("°C");
+
+  // Sensor deactiveren
+  temp_sensor.shutdown(true);
+}
+
+/*
+   Lichtsensor
+*/
+void getLuminance() {
+  // Meet voltage op analoge input en reken dit om in het equivalente aantal lux
+  float volts = analogRead(A0) * 3.3 / 1024.0;
+  float amps = volts / 10000.0;  // 10k weerstand
+  float microamps = amps * 1000000;
+  float lux = microamps * 2.0;
+
+  Serial.print("Hoevelheid licht: ");
+  Serial.print(lux);
+  Serial.println(" lux");
+}
+
+/*
+   Reguliere led
+*/
+void setLed(bool state) {
+  digitalWrite(LED_PIN, !state);
+}
+
+/*
+   RGB leds
+*/
+void setRgbLeds(CRGB color) {
+  for (int i = 0; i < RGB_NUM_LEDS; i++) {
+    setRgbLed(i, color);
+  }
+}
+
+void setRgbLed(int ledno, CRGB color) {
+  leds[ledno] = color;
+  FastLED.show();
+}
+
+/*
+   Deze functie wordt eenmalig aangroepen bij opstarten
+*/
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+  Serial.println();
+
+  // Knop instellen
+  pinMode(BUTTON_PIN, INPUT);
+  button.setClickTicks(300);
+  button.setPressTicks(600);
+  button.attachClick(singleClick);
+  button.attachDoubleClick(doubleClick);
+  button.attachLongPressStart(longPress);
+
+  // Start I2C voor temperatuursensor
+  Wire.begin();
+
+  // Reguliere led instellen
+  pinMode(LED_PIN, OUTPUT);
+  setLed(false); // led uit
+
+  // Initialiseer RGB leds
+  FastLED.addLeds<RGB_LED_TYPE, RGB_LED_DATA_PIN, RGB_LED_CLOCK_PIN, RGB_COLOR_ORDER>(leds, RGB_NUM_LEDS); //.setCorrection(TypicalLEDStrip);
+  FastLED.setBrightness(RGB_LED_BRIGHTNESS);
+  setRgbLeds(CRGB::Black); // beide leds uit
+
+  // Wifi instellen
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Wifi verbinden");
+
+  // Wachten tot wifi verbonden is
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println(" verbonden!");
+  Serial.print("IP: ");
+  Serial.println(WiFi.localIP());
+
+  // Lees beide sensoren uit
+  getTemperature();
+  getLuminance();
+}
+
+/**
+   Functie wordt continue aangroepen, voeg hier je eigen logica aan toe
+*/
+void loop() {
+  // Laat knop controleren of er activiteit geweest is
+  button.tick();
+}
